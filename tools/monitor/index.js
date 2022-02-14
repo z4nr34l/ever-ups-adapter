509699aec44f1a4d1e129a22bcc31d20355d905d
Mateusz Janota <software@zanreal.pl>
2022-02-14 15:57:26 +0100

    Added simple monitoring tool for live testing UPS devices

---
+++ tools/monitor/index.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022.
+ * Author: Mateusz "Z4NR34L" Janota
+ * Author URI: https://www.zanreal.pl/
+ * Author email: software@zanreal.pl
+ */
+import { SnmpAdapter } from "ever-ups-adapter"
+import {Transform} from "node:stream"
+import {Console} from "node:console"
+import logUpdate from "log-update"
+
+const ts = new Transform({ transform(chunk, enc, cb) { cb(null, chunk) } })
+const logger = new Console({ stdout: ts })
+
+const getTable = (data) => {
+  logger.table(data)
+  return (ts.read() || '').toString()
+}
+
+setInterval(async () => {
+  const session = new SnmpAdapter({
+    address: "10.0.0.222",
+    community: "public"
+  })
+
+  await session.getAllData().then(data => logUpdate(getTable(data))).catch(error => logUpdate(error))
+}, 80)
